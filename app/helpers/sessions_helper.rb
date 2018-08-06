4ee82734c22619410a0dca54f8e15b9be9e12348
Kelley Yonezaki <kelley.yonezaki@key.me>
2018-08-06 10:30:07 -0400

    inital push

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  #Remembers a user in a persistent session.
+  # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
   end
 
   # Returns the current logged-in user (if any).
@@ -56,5 +61,16 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
